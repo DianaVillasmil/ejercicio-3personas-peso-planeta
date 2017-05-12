d99ad6034f5e56d5541058e696099bc2afea2d0f
Diana Villasmil <diacavipi@gmail.com>
2017-05-11 23:17:46 -0300

    inicio peso-planeta

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+var gravedades = [3.7, 8.87, 9.8, 3.71, 24.79, 10.44, 8.69, 11.15, 0.62];
+var planetas = ["Mercurio", "Venus", "Tierra", "Marte", "Jupiter", "Saturno", "Urano", "Neptuno", "Pluton"];
+
+
